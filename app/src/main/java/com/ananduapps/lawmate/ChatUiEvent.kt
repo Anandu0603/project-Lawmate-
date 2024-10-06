9f17d26302d8cc1ffd6a2521f890f57f22585dcc
Anandu Suresh <149881625+Anandu0603@users.noreply.github.com>
2024-10-06 14:56:14 +0530

    Update ChatUiEvent.kt

--- app/src/main/java/com/ananduapps/lawmate/ChatUiEvent.kt
+++ app/src/main/java/com/ananduapps/lawmate/ChatUiEvent.kt
@@ -2,9 +2,7 @@ package com.ananduapps.lawmate
 
 import android.graphics.Bitmap
 
-/**
- * @author Ahmed Guedmioui
- */
+
 sealed class ChatUiEvent {
     data class UpdatePrompt(val newPrompt: String) : ChatUiEvent()
     data class SendPrompt(
