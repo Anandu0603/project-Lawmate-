d778740677bc79aba82c507da75cda6e21598828
Anandu Suresh <149881625+Anandu0603@users.noreply.github.com>
2024-10-06 14:56:37 +0530

    Update ChatState.kt

--- app/src/main/java/com/ananduapps/lawmate/ChatState.kt
+++ app/src/main/java/com/ananduapps/lawmate/ChatState.kt
@@ -3,9 +3,7 @@ package com.ananduapps.lawmate
 import android.graphics.Bitmap
 import com.ananduapps.lawmate.data.Chat
 
-/**
- * @author Ahmed Guedmioui
- */
+
 data class ChatState (
     val chatList: MutableList<Chat> = mutableListOf(),
     val prompt: String = "",
